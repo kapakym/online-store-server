19e1e6a0e0ef076464661662583448fbabce19cc
Alex P <mycop14@mail.ru>
2022-06-27 15:55:01 +0300

    Прописал логику работы функции CreateFile

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -10,6 +10,18 @@ import * as uuid from 'uuid';
 export class FilesService {
   async createFile(file): Promise<string> {
     try {
+      // Генерируем уникальное имя файла
+      const fileName = uuid.v4() + '.jpg';
+      // Формируем путь, где будут храниться картинки
+      const filePath = path.resolve(__dirname, '..', 'static');
+      if (!fs.existsSync(filePath)) {
+        // Если папка не существует, то создаем ее
+        fs.mkdirSync(filePath, { recursive: true });
+      }
+      // Записываем файл на диск
+      fs.writeFileSync(path.join(filePath, fileName), file.buffer);
+      // Возращаем название файла
+      return fileName;
     } catch (error) {
       throw new HttpException(
         'Произошла ошибка при записи файла',
