86ee74db182614940918ffdb210d9f3150afdf7f
Alex P <mycop14@mail.ru>
2022-08-16 15:23:15 +0300

    Инжектировал properyRepository

--- src/templates/template.service.ts
+++ src/templates/template.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { Template } from './template.model';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreateTemplateDto } from './dto/create-template.dto';
+import { Property } from './property.model';
+import { CreateTemplatePropertysDto } from './dto/create-template-propertys.dto';
+import { GetTemplateByPageDto } from './dto/get-template-by-page.dto';
 
 @Injectable()
 export class TemlateService {
   constructor(
     @InjectModel(Template) private templateRepository: typeof Template,
+    @InjectModel(Property) private propertyRepository: typeof Property,
   ) {}
 
   async createTemplate(dto: CreateTemplateDto) {
@@ -17,5 +21,23 @@ export class TemlateService {
   async getAllTemplates() {
     const templates = await this.templateRepository.findAll();
     return templates;
+  }
+
+  async createProperty(dto: CreateTemplatePropertysDto) {
+    for (const element of dto.propertys) {
+      const property = this.propertyRepository.create(element);
+    }
+    return { message: 'OK' };
+  }
+
+  async getTemplatesByPage(dto: GetTemplateByPageDto) {
+    const count = await this.templateRepository.count();
+    const templates = await this.templateRepository.findAll({
+      include: { all: true },
+      limit: dto.limit,
+      offset: dto.limit * dto.page,
+      order: [['name', 'ASC']],
+    });
+    return { templates, count };
   }
 }
