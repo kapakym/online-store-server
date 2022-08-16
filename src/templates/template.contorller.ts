6d0efaaeafd14d0d2e765b6b5ed5e832d78cc45d
Alex P <mycop14@mail.ru>
2022-08-16 15:22:35 +0300

    Добавил контроллеры для создания свойств у шаблонов и получения определенного количества записей

--- src/templates/template.contorller.ts
+++ src/templates/template.contorller.ts
@@ -1,14 +1,33 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { TemlateService } from './template.service';
 import { CreateTemplateDto } from './dto/create-template.dto';
+import { CreateTemplatePropertysDto } from './dto/create-template-propertys.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { GetTemplateByPageDto } from './dto/get-template-by-page.dto';
 
 @Controller('/template')
 export class TemplateContorller {
   constructor(private templateService: TemlateService) {}
 
+  @ApiOperation({ summary: 'Создание шаблона' })
+  @ApiResponse({ status: 200, type: CreateTemplateDto })
   @Post()
   create(@Body() dto: CreateTemplateDto) {
     return this.templateService.createTemplate(dto);
+  }
+
+  @ApiOperation({ summary: 'Создание свойств' })
+  @ApiResponse({ status: 200, type: CreateTemplatePropertysDto })
+  @Post('/property')
+  cretaeProperty(@Body() dto: CreateTemplatePropertysDto) {
+    return this.templateService.createProperty(dto);
+  }
+
+  @ApiOperation({ summary: 'Получения определенного количества шаблонов' })
+  @ApiResponse({ status: 200, type: CreateTemplatePropertysDto })
+  @Get('/page')
+  getTemplatesByPage(@Query() dto: GetTemplateByPageDto) {
+    return this.templateService.getTemplatesByPage(dto);
   }
 
   @Get()
