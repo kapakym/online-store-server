84f3795a8d8fae3814f2269536b7f91e03f03ada
Alex P <mycop14@mail.ru>
2022-08-16 15:22:53 +0300

    Импортировал модель свойств

--- src/templates/template.module.ts
+++ src/templates/template.module.ts
@@ -3,10 +3,12 @@ import { TemplateContorller } from './template.contorller';
 import { TemlateService } from './template.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Template } from './template.model';
+import { Property } from './property.model';
 
 @Module({
   controllers: [TemplateContorller],
   providers: [TemlateService],
-  imports: [SequelizeModule.forFeature([Template])],
+  // Импортировал в модуль модели для работы с Шаблонами и Свойствами
+  imports: [SequelizeModule.forFeature([Template, Property])],
 })
 export class TemplateModule {}
