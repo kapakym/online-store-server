a9d6ea4b311b20bc73d82cb213c0479cf7d04cfb
Alex P <mycop14@mail.ru>
2022-08-17 15:10:00 +0300

    Добавил сервис для получения списка свойств определенного шаблона

--- src/templates/template.service.ts
+++ src/templates/template.service.ts
@@ -3,8 +3,9 @@ import { Template } from './template.model';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreateTemplateDto } from './dto/create-template.dto';
 import { Property } from './property.model';
-import { CreateTemplatePropertysDto } from './dto/create-template-propertys.dto';
 import { GetTemplateByPageDto } from './dto/get-template-by-page.dto';
+import { GetPropertyByPageDto } from './dto/get-property-by-page.dto';
+import { CreateTemplatePropertyDto } from './dto/create-template-property.dto';
 
 @Injectable()
 export class TemlateService {
@@ -23,16 +24,28 @@ export class TemlateService {
     return templates;
   }
 
-  async createProperty(dto: CreateTemplatePropertysDto) {
-    for (const element of dto.propertys) {
-      const property = this.propertyRepository.create(element);
-    }
+  async createProperty(dto: CreateTemplatePropertyDto) {
+    const property = await this.propertyRepository.create(dto);
     return { message: 'OK' };
   }
 
   async getTemplatesByPage(dto: GetTemplateByPageDto) {
     const count = await this.templateRepository.count();
     const templates = await this.templateRepository.findAll({
+      include: { all: true },
+      limit: dto.limit,
+      offset: dto.limit * dto.page,
+      order: [['name', 'ASC']],
+    });
+    return { templates, count };
+  }
+
+  async getPropertyByPage(dto: GetPropertyByPageDto) {
+    const count = await this.propertyRepository.count({
+      where: { templateId: dto.templateId },
+    });
+    const templates = await this.propertyRepository.findAll({
+      where: { templateId: dto.templateId },
       include: { all: true },
       limit: dto.limit,
       offset: dto.limit * dto.page,
