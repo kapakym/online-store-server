14d4b59fd82926258e25dd95e9dc3950d02bcf11
Alex P <mycop14@mail.ru>
2022-08-17 13:18:46 +0300

    ДОбавил описания к свойствам

--- src/templates/dto/create-template.dto.ts
+++ src/templates/dto/create-template.dto.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateTemplateDto {
+  @ApiProperty({ example: 'Высота (м)', description: 'Имя свойства товара' })
   readonly name;
 }
