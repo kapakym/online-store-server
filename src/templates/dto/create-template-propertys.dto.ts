f9e279c8a202020536d11d9118f6ba21c7694c6b
Alex P <mycop14@mail.ru>
2022-08-21 00:18:59 +0300

    Добавил новые свойства

--- src/templates/dto/create-template-propertys.dto.ts
+++ src/templates/dto/create-template-propertys.dto.ts
@@ -2,6 +2,7 @@ import { CreateTemplatePropertyDto } from './create-template-property.dto';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTemplatePropertysDto {
+  readonly templateId: number;
   @ApiProperty({ example: 'Массив объектов', description: 'Массив свойств' })
   readonly propertys: Array<CreateTemplatePropertyDto>;
 }
