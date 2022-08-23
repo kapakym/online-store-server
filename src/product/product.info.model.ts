708311e348dc1e964cec19f33dba380678de27f4
Alex P <mycop14@mail.ru>
2022-08-23 11:51:27 +0300

    Модель в которой будет содержаться свойства товара

---
+++ src/product/product.info.model.ts
@@ -0,0 +1,42 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Table({ tableName: 'product-info' })
+export class ProductInfo extends Model<ProductInfo> {
+  @ApiProperty({
+    example: '1',
+    description: 'Уникальный идентефикатор свойства товара',
+  })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id: number;
+
+  @ApiProperty({
+    example: 'строка',
+    description: 'Тип свойства [строка, число, ссылка]',
+  })
+  @Column({
+    type: DataType.STRING,
+  })
+  type: string;
+  @ApiProperty({
+    example: 'Высота (м)',
+    description: 'Имя свойства',
+  })
+  @Column({
+    type: DataType.STRING,
+  })
+  name: string;
+  @ApiProperty({
+    example: '100',
+    description: 'Значение свойства',
+  })
+  @Column({
+    type: DataType.STRING,
+  })
+  value: string;
+}
