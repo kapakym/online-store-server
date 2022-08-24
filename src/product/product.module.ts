fd97afdb3be51465b66d59804bd0be1a461c228c
Alex P <mycop14@mail.ru>
2022-08-24 13:38:27 +0300

    Модуль для работы с продуктами

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -6,12 +6,18 @@ import { Product } from './product.model';
 import { ProductInfo } from './product.info.model';
 import { ProductPicture } from './product.picture.model';
 import { FilesModule } from '../files/files.module';
+import { Property } from '../templates/property.model';
 
 @Module({
   controllers: [ProductController],
   providers: [ProductService],
   imports: [
-    SequelizeModule.forFeature([Product, ProductInfo, ProductPicture]),
+    SequelizeModule.forFeature([
+      Product,
+      ProductInfo,
+      ProductPicture,
+      Property,
+    ]),
     FilesModule,
   ],
 })
