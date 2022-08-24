c8cfb49048b561b9175b65ca2dac4122019bbdd6
Alex P <mycop14@mail.ru>
2022-08-24 13:37:26 +0300

    Контроллер для работы с продуктами

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { CreateProductDto } from './dto/create-product.dto';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { GetProductByPageDto } from './dto/get-product-by-page.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @ApiOperation({ summary: 'Добавление продукта' })
+  @ApiResponse({ status: 200, type: CreateProductDto })
+  @Post()
+  @UseInterceptors(FilesInterceptor('photo'))
+  create(@Body() dto: any, @UploadedFiles() photo) {
+    console.log(dto, photo.length);
+    return this.productService.createProduct(dto, photo);
+  }
+
+  @ApiOperation({ summary: 'Получение опредленного количества продуктов' })
+  @ApiResponse({ status: 200, type: GetProductByPageDto })
+  @Get('/page')
+  getProductByPage(@Query() dto: GetProductByPageDto) {
+    return this.productService.getProductByPage(dto);
+  }
+}
