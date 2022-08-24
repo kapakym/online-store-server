c735687c1061448f9e8c624bec1968e70395bac9
Alex P <mycop14@mail.ru>
2022-08-24 13:39:14 +0300

    Сервис для работы с продуктами

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -4,6 +4,9 @@ import { Product } from './product.model';
 import { ProductInfo } from './product.info.model';
 import { ProductPicture } from './product.picture.model';
 import { FilesService } from '../files/files.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Property } from '../templates/property.model';
+import { GetProductByPageDto } from './dto/get-product-by-page.dto';
 
 @Injectable()
 export class ProductService {
@@ -12,6 +15,43 @@ export class ProductService {
     @InjectModel(ProductInfo) private productInfoRepository: typeof ProductInfo,
     @InjectModel(ProductPicture)
     private productPictureRepository: typeof ProductPicture,
+    @InjectModel(Property) private propertyRepository: typeof Property,
     private fileService: FilesService,
   ) {}
+
+  async getProductByPage(dto: GetProductByPageDto) {
+    const count = await this.propertyRepository.count();
+    const templates = await this.productRepository.findAll({
+      include: { all: true },
+      limit: dto.limit,
+      offset: dto.limit * dto.page,
+      order: [['name', 'ASC']],
+    });
+    return { templates, count };
+  }
+
+  async createProduct(dto: CreateProductDto, photo: any) {
+    const product = await this.productRepository.create(dto);
+    for (const item of photo) {
+      const filename = await this.fileService.createFile(item);
+      console.log(filename);
+      const newPhoto = await this.productPictureRepository.create({
+        productId: product.id,
+        filename: filename,
+      });
+    }
+    const propertys = await this.propertyRepository.findAll({
+      where: { templateId: dto.templateId },
+    });
+    for (const item of propertys) {
+      console.log('item----->', item.name);
+      const newInfo = await this.productInfoRepository.create({
+        productId: product.id,
+        name: item.name,
+        type: item.type,
+      });
+    }
+
+    // for (const item in photo) console.log(item.name);
+  }
 }
